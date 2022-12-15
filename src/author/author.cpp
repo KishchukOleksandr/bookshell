c591b479b7a68bea9a375e39c6cfad8e8a5acb86
Oleksandr Kishchuk <olexandrino@gmail.com>
2022-12-15 10:23:38 +0200

    add bio methods

--- src/author/author.cpp
+++ src/author/author.cpp
@@ -8,3 +8,13 @@ Author::Author(const string &fName, const string &sName)
 {
 	cout << "New author" << firstName << " " << secondName << " is created";	
 }
+
+string Author::Bio() const
+{
+	return bio;
+}
+
+void Author::Bio(const string &bio)
+{
+	this->bio = bio;
+}
